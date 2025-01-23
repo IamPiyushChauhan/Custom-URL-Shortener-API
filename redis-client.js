bc3824d78f84fffeaebdaa659ade39aea2e24ffb
IamPiyushChauhan <130564688+IamPiyushChauhan@users.noreply.github.com>
2025-01-23 21:30:15 +0530

    removed uncatented comment

--- redis-client.js
+++ redis-client.js
@@ -1,12 +1,8 @@
-// const Redis = require('ioredis')
-// const redis = new Redis()
-// module.exports = redis
 
+require('dotenv') 
 
-require('dotenv') // Load environment variables from .env file
 const Redis = require('ioredis')
 
-// Use the REDIS_URL from the environment variable
 const redis = new Redis(process.env.REDIS_URL)
 
 module.exports = redis
