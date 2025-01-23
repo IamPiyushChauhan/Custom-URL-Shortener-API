93462305dcacdd1c4add0dd1982afe632daa959f
IamPiyushChauhan <130564688+IamPiyushChauhan@users.noreply.github.com>
2025-01-23 22:11:45 +0530

    added getDeviceType and getOsType to get device type and os type from userAgent String

--- utils/apiUtils.js
+++ utils/apiUtils.js
@@ -13,11 +13,29 @@ function isValidateUrl(url) {
     }
 }
 
+const getDeviceType = (userAgent) => {
+    if (userAgent.includes('Mobi') || userAgent.includes('Android') || userAgent.includes('iPhone') || userAgent.includes('android')|| userAgent.includes('iphone')) {
+        return 'mobile';
+    } else if (userAgent.includes('Macintosh') || userAgent.includes('Windows') || userAgent.includes('Linux') || userAgent.includes('Windows') || userAgent.includes('Win64') || userAgent.includes('Win32') || userAgent.includes('macOS') || userAgent.includes('Macintosh' || userAgent.includes('windows')) || userAgent.includes('Window')) {
+        return 'desktop';
+    }
+    return 'unknown';
+};
 
+const getOsType = (userAgent) => {
+    if (userAgent.includes('Android') || userAgent.includes('android')) return 'android';
+    if (userAgent.includes('iPhone') || userAgent.includes('iphone')) return 'iOS';
+    if (userAgent.includes('Windows') || userAgent.includes('Win64') || userAgent.includes('Win32') || userAgent.includes('windows') || userAgent.includes('window') || userAgent.includes('Window')) return 'windows';
+    if (userAgent.includes('Mac OS X') || userAgent.includes('macOS') || userAgent.includes('Macintosh') || userAgent.includes('macintosh') || userAgent.includes('mac')) return 'macOS'; 
+    if (userAgent.includes('Linux') || userAgent.includes('linux')) return 'linux';
+    return 'Unknown';
+};
 
 
 
 module.exports ={
     generateShortUrl,
-    isValidateUrl
+    isValidateUrl,
+    getDeviceType,
+    getOsType,
 }
