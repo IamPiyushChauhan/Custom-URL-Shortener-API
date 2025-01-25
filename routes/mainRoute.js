9284c933361fe2379ec17e413ef9731a16b7be2e
IamPiyushChauhan <130564688+IamPiyushChauhan@users.noreply.github.com>
2025-01-25 12:57:28 +0530

    added healthz endpoint for render.com for Health Checks

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -16,6 +16,7 @@ router.get('/success' , userController.successGoogleLogin);
 router.get('/failure' , userController.failureGoogleLogin);
 
 router.get('/healthz', (req, res) => {
+    
     res.status(200).json({
         status: 'ok',
         timestamp: new Date().toISOString(),
